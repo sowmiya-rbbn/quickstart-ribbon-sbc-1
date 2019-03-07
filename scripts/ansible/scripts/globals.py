585d012dbe9d417dc221263d98b7bd80b86a745f
Andrew Glenn <andglenn@amazon.com>
2019-03-07 14:18:11 -0600

    more minor tweaks

--- scripts/ansible/scripts/globals.py
+++ scripts/ansible/scripts/globals.py
@@ -1,16 +1,10 @@
 #!/usr/bin/env python
-import os
-import os.path
 
-cwd = os.getcwd()
-print(cwd)
-Pdir = os.path.abspath(os.path.join(cwd, os.pardir))
-                        
+Pdir = "/root/ansible"
+
 LOG_FILENAME                   =  Pdir+'/ConfigGen.log'
 LOGGER_NAME                    = 'ConfigGen'
-CONFIG_DATA_FILE	       = '/root.input.json'
+CONFIG_DATA_FILE	       = '/root/input.json'
 VAR_CONF_FILE		       = Pdir+'/vars/gen.yml'
 PAYLOAD_LOC		       = Pdir+'/payload'
 GEN_LOC			       = Pdir+'/payload'
-
-
